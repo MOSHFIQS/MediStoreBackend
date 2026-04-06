6bf82a96b43a94565b508fc860226645dc2e397c
MOSHFIQS <moshfiqs.dev@gmail.com>
2026-04-06 12:02:06 +0600

    feat:  coupon module integrated

---
+++ src/modules/coupon/coupon.controller.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from "express"
+import status from "http-status"
+import sendResponse from "../../utils/sendResponse"
+import { couponService } from "./coupon.service"
+
+const createCoupon = async (req: Request, res: Response, next: NextFunction) => {
+     try {
+          const result = await couponService.createCoupon(req.body)
+          sendResponse(res, { statusCode: status.CREATED, success: true, message: "Coupon created", data: result })
+     } catch (e) { next(e) }
+}
+
+const getAllCoupons = async (req: Request, res: Response, next: NextFunction) => {
+     try {
+          const result = await couponService.getAllCoupons()
+          sendResponse(res, { statusCode: status.OK, success: true, message: "Coupons fetched", data: result })
+     } catch (e) { next(e) }
+}
+
+const updateCoupon = async (req: Request, res: Response, next: NextFunction) => {
+     try {
+          const result = await couponService.updateCoupon(req.params.id as string, req.body)
+          sendResponse(res, { statusCode: status.OK, success: true, message: "Coupon updated", data: result })
+     } catch (e) { next(e) }
+}
+
+const deleteCoupon = async (req: Request, res: Response, next: NextFunction) => {
+     try {
+          await couponService.deleteCoupon(req.params.id as string)
+          sendResponse(res, { statusCode: status.OK, success: true, message: "Coupon deleted", data: null })
+     } catch (e) { next(e) }
+}
+
+const validateCoupon = async (req: Request, res: Response, next: NextFunction) => {
+     try {
+          const { code, orderAmount } = req.body
+          const result = await couponService.validateCoupon(code, parseFloat(orderAmount))
+          sendResponse(res, { statusCode: status.OK, success: true, message: "Coupon valid", data: result })
+     } catch (e) { next(e) }
+}
+
+export const couponController = { createCoupon, getAllCoupons, updateCoupon, deleteCoupon, validateCoupon }
